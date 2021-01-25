4f10d42d2210b35077e720d77690c519f8ea1566
User2573 <71093251+User2573@users.noreply.github.com>
2021-01-25 12:05:35 -0500

    Create main.js

---
+++ test/main.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  //fit canvas to window and fix issues with canvas blur on zoom
+  canvas.style.width = window.innerWidth + "px";
+  canvas.style.height = window.innerHeight + "px";
+  const scale = window.devicePixelRatio;
+  canvas.width = window.innerWidth * scale;
+  canvas.height = window.innerHeight * scale;
+  ctx.scale(scale, scale);
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+
+function cycle(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.beginPath();
+  ctx.moveTo(0, 0)
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.stroke();
+  ctx.closePath();
+  
+  requestAnimationFrame(cycle);
+}
+requestAnimationFrame(cycle);
