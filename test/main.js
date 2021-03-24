17eee46c88336cdb88ee5e0e06e524c4bedc4058
User2573 <71093251+User2573@users.noreply.github.com>
2021-03-24 12:53:05 -0400

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+
 function resizeCanvas() {
   //fit canvas to window and fix issues with canvas blur on zoom
   canvas.style.width = window.innerWidth + "px";
@@ -9,6 +12,8 @@ function resizeCanvas() {
   canvas.width = window.innerWidth * scale;
   canvas.height = window.innerHeight * scale;
   ctx.scale(scale, scale);
+  centerX = canvas.width / 2;
+  centerY = canvas.height/2;
 }
 resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
@@ -22,6 +27,9 @@ function cycle(){
   ctx.lineTo(canvas.width, canvas.height);
   ctx.stroke();
   ctx.closePath();
+  
+  ctx.arc(centerX, centerY, 0, 2 * Math.PI);
+  ctx.stroke();
   
   requestAnimationFrame(cycle);
 }
